600a466be93bba6bbc86b6f2b7551be40aa47ccc
Ruki Fujiwara <rukiwillgotoksf@gmail.com>
2020-05-13 02:13:32 +0900

    akonrov

--- Nabeatsu/ViewController.swift
+++ Nabeatsu/ViewController.swift
@@ -14,61 +14,51 @@ class ViewController: UIViewController {
     var number: Int = 0
     @IBOutlet var countLabel: UILabel!
     @IBOutlet var faceLabel: UILabel!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-    var line: Array = ["まだやるの？","やりすぎ笑","ウケる笑笑","辞めな！！"]
-        func isAho() -> Bool {
+    
+    func isAho() -> Bool {
         // 問題1: 3の倍数かどうか調べる
-        if number%3 == 0 {
+        if number % 3 == 0 {
             return true
         }
-        // 問題2: 1の位が3かどうか調べる
-        if number%10 == 3{
-            return true
-        }
-        // 問題3: 10の位が3かどうか調べる
-        if number/10%10 == 3 {
-            return true
-        }
-        // 問題4: 3がつくかどうか調べる
-        var checknum: Int = number
-        while checknum != 0 {
-            if checknum % 10 == 3 {
-                return true
-            }else {
-                checknum = checknum/10
-            }
-        }
-        // 問題4をやるときは問題3と問題2の答えを消してから書こう
         
-        return false
-    }
-    func ii() -> Bool{
-        if number % 50 == 0{
+    var checkNum: Int = number
+       
+       while checkNum != 0 {
+       if  checkNum % 10 == 3 {
             return true
-        }
-        return false
+       }
+       else {
+       checkNum = checkNum/10
+       }
+       }
+    
+    
+    return false
     }
-    @IBAction func pluscount(){
+    
+   @IBAction func plusButton(){
         number = number + 1
         countLabel.text = String(number)
         
-       
-        if ii() == true {
-          faceLabel.text = line[Int.random(in:0..<line.count)]
-        }else  if isAho() == true {
-                   
-                   faceLabel.text = "ﾍ(ﾟ∀ﾟﾍ)ｱﾋｬ"
-               } else {
-                   
-                   faceLabel.text = "(゜o゜)"
-               }
+        if isAho() == true {
+            
+            faceLabel.text = "ﾍ(ﾟ∀ﾟﾍ)ｱﾋｬ"
+        } else {
+            
+            faceLabel.text = "(゜o゜)"
         }
-    @IBAction func clear() {
-           number  = number*0
-       }
-
     }
+    
+    @IBAction func clear(){
+    number = 0
+    countLabel.text = String(number)
+    faceLabel.text = "(゜o゜)"
+    }
+
+    
+}
